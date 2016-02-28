fcdd7d71845f0e142e3b71748592bb384698d2d7
dannimayo <dalvarez.mayorga@gmail.com>
2016-02-28 13:43:55 -0600

    First Commit to private Repo
    First Commit to private Repo

--- api/models/Customer.js
+++ api/models/Customer.js
@@ -1,13 +1,37 @@
 /**
- * Customer.js
- *
- * @description :: TODO: You might write a short summary of how this model works and what it represents here.
- * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
- */
+* Customer.js
+*
+* @description :: Customer contact information stored here
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
 
 module.exports = {
 
+  schema: true,
+
   attributes: {
+    name: {
+      type: 'string',
+      required: true,
+    },
+    street_address: {
+      type: 'string',
+    },
+    city: {
+      type: 'string'
+    },
+    state: {
+      type: 'string'
+    },
+    email: {
+      type: 'string',
+      email: true
+    },
+
+    stocks: {
+      collection: 'stock',
+      via: 'owner'
+    }
 
   }
 };
